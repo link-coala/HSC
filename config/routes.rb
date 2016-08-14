2c73535b058f233d4d98f1511060981a79b75964
link-coala <camposcoala@gmail.com>
2016-08-14 06:13:38 -0400

    ruta raiz

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/caracteristicas'
+  root 'static_pages#home'
 
   get 'static_pages/creditos'
 
-  get 'static_pages/home'
+  get 'static_pages/caracteristicas'
 
 end
