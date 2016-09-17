c0fee8b2bbcf89b7f8129ee36156f0f84679d0b6
link-coala <camposcoala@gmail.com>
2016-09-17 13:02:39 -0500

    usuarios with log out error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'static_pages#home'
   get 'creditos' => 'static_pages#creditos'
   get 'caracteristicas' => 'static_pages#caracteristicas'
