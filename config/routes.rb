46dd39e808152d76cbdcc0d39061d3179807fa8c
link-coala <camposcoala@gmail.com>
2016-09-17 15:15:10 -0500

    logout fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root 'static_pages#home'
   get 'creditos' => 'static_pages#creditos'
   get 'caracteristicas' => 'static_pages#caracteristicas'
